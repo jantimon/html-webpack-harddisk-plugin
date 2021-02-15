d7aa15dde5c8c1cc6f061376c25be59ad5b26c0e
Jan Nicklas <j.nicklas@me.com>
2021-02-15 09:31:11 +0100

    feat: add support for webpack 5
    BREAKING CHANGE: drop support for node < 10.16 and webpack 1-3

--- index.js
+++ index.js
@@ -1,66 +1,51 @@
 'use strict';
-var mkdirp = require('mkdirp');
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-function HtmlWebpackHarddiskPlugin (options) {
-  options = options || {};
-  this.outputPath = options.outputPath;
-}
+class HtmlWebpackHarddiskPlugin {
+  constructor (options) {
+    this.options = options || {};
+    this.outputPath = this.options.outputPath;
+  }
 
-HtmlWebpackHarddiskPlugin.prototype.apply = function (compiler) {
-  var self = this;
+  apply (compiler) {
+    compiler.hooks.compilation.tap('HtmlWebpackHarddisk', (compilation) => {
+      // HtmlWebPackPlugin 4.x
+      const HtmlWebpackPlugin = require('html-webpack-plugin');
+      const hooks = HtmlWebpackPlugin.getHooks(compilation);
 
-  if (compiler.hooks) {
-    // webpack 4 support
-    compiler.hooks.compilation.tap('HtmlWebpackHarddisk', function (compilation) {
-      if (compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration) {
-        compilation.hooks.htmlWebpackPluginAfterEmit.tapAsync('HtmlWebpackHarddisk', function (htmlPluginData, callback) {
-          self.writeAssetToDisk(compilation, htmlPluginData.plugin.options, htmlPluginData.outputName, callback);
-        });
-      } else {
-        // HtmlWebPackPlugin 4.x
-        var HtmlWebpackPlugin = require('html-webpack-plugin');
-        var hooks = HtmlWebpackPlugin.getHooks(compilation);
-
-        hooks.afterEmit.tapAsync('HtmlWebpackHarddisk', function (htmlPluginData, callback) {
-          self.writeAssetToDisk(compilation, htmlPluginData.plugin.options, htmlPluginData.outputName, callback);
-        });
-      }
-    });
-  } else {
-    // webpack 3 support
-    compiler.plugin('compilation', function (compilation) {
-      compilation.plugin('html-webpack-plugin-after-emit', function (htmlPluginData, callback) {
-        self.writeAssetToDisk(compilation, htmlPluginData.plugin.options, htmlPluginData.outputName, callback);
+      hooks.afterEmit.tapAsync('HtmlWebpackHarddisk', (htmlPluginData, callback) => {
+        this.writeAssetToDisk(compilation, htmlPluginData.plugin.options, htmlPluginData.outputName, callback);
       });
     });
   }
-};
 
-/**
- * Writes an asset to disk
- */
-HtmlWebpackHarddiskPlugin.prototype.writeAssetToDisk = function (compilation, htmlWebpackPluginOptions, webpackHtmlFilename, callback) {
-  // Skip if the plugin configuration didn't set `alwaysWriteToDisk` to true
-  if (!htmlWebpackPluginOptions.alwaysWriteToDisk) {
-    return callback(null);
-  }
-  // Prepare the folder
-  var fullPath = path.resolve(this.outputPath || compilation.compiler.outputPath, webpackHtmlFilename);
-  var directory = path.dirname(fullPath);
-  mkdirp(directory, function (err) {
-    if (err) {
-      return callback(err);
+  /**
+   * Writes an asset to disk
+   */
+  writeAssetToDisk (compilation, htmlWebpackPluginOptions, webpackHtmlFilename, callback) {
+    // Skip if the plugin configuration didn't set `alwaysWriteToDisk` to true
+    if (!htmlWebpackPluginOptions.alwaysWriteToDisk) {
+      return callback(null);
     }
-    // Write to disk
-    fs.writeFile(fullPath, compilation.assets[webpackHtmlFilename].source(), function (err) {
+    // Prepare the folder
+    const fullPath = path.resolve(this.outputPath || compilation.compiler.outputPath, webpackHtmlFilename);
+    const directory = path.dirname(fullPath);
+
+    fs.mkdir(directory, { recursive: true }, function (err) {
       if (err) {
         return callback(err);
       }
-      callback(null);
+      // Write to disk
+      fs.writeFile(fullPath, compilation.assets[webpackHtmlFilename].source(), function (err) {
+        if (err) {
+          return callback(err);
+        }
+        callback(null);
+      });
     });
-  });
-};
+  }
+}
 
 module.exports = HtmlWebpackHarddiskPlugin;
+module.exports.HtmlWebpackHarddiskPlugin = HtmlWebpackHarddiskPlugin;
